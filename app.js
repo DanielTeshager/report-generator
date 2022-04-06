5c0250cfeb32e0d666a03c9a8b188a24a9c46a4c
Daniel <dwolela@gmail.com>
2022-04-06 16:20:20 +0400

    updated webapp.py cors url

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ async function postData(url = '', data = {}, method = 'POST') {
     // Default options are marked with *
     const response = await fetch(url, {
       method: method, // *GET, POST, PUT, DELETE, etc.
-      mode: 'no-cors', // no-cors, *cors, same-origin
+    //   mode: 'no-cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     //   credentials: 'same-origin', // include, *same-origin, omit
       headers: {
